cf1c59bb437ba9d9ef44d9239ac48a0869fe2584
Lucas <Lucas@LAPTOP-56FIM35R>
2018-05-09 22:20:38 -0300

    variables operacionales completo

---
+++ Variables_operaciones/Variables_operaciones/Eje1_16.vb
@@ -0,0 +1,15 @@
+﻿Module Eje1_16
+    Sub main()
+        Dim fecha As Date
+        Dim numDias As Byte
+
+        Console.Write("Ingrese una fecha DD,MM,AAAA: ")
+        fecha = Console.ReadLine()
+        Console.Write("Ingrese el numero de dias para agregar: ")
+        numDias = Console.ReadLine()
+
+        Console.WriteLine("Fecha Resultado: " & fecha.AddDays(numDias))
+
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Variables_operaciones/Variables_operaciones/Eje1_17.vb
@@ -0,0 +1,30 @@
+﻿Module Eje1_17
+    Enum DiasSemana
+        Domingo = 0
+        Lunes = 1
+        Martes = 2
+        Miercoles = 3
+        Jueves = 4
+        Viernes = 5
+        Sabado = 6
+    End Enum
+
+    Sub main()
+        Dim fecha As Date
+        Dim numDias As Byte
+
+        Console.Write("Ingrese una fecha DD,MM,AAAA: ")
+        fecha = Console.ReadLine()
+        Console.Write("Ingrese el numero de dias para agregar: ")
+        numDias = Console.ReadLine()
+
+        fecha = fecha.AddDays(numDias)
+
+        Console.WriteLine("Fecha Resultado: " & fecha)
+
+        Dim diaSemana As DiasSemana = fecha.DayOfWeek
+        Console.WriteLine("Dia correspondiente: " & diaSemana.ToString) 'toString para ver la etiqueta de la unumeraion
+
+        Console.ReadKey()
+    End Sub
+End Module
